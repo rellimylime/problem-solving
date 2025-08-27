5b8db08abe08f854538ac49f9a79b68df1329579
rellimylime <ermiller@ucsb.edu>
2025-08-27 11:30:46 -0700

    tiny problem fun

--- problem-solving.R
+++ problem-solving.R
@@ -19,23 +19,42 @@
 
 # What is the list of variables you need to solve the problem for _one_ case?
 
+# Sample_Date
+# Chemical + concentration
+# (window size)
 
 # 4. Solution in a Function -----------------------------------------------
 
 # Write a function that takes the minimum information as parameters
 
-your_function <- function(param1, param2, param3) {
-  do_stuff <- param1 + param2 + param3
-  return(do_stuff)
+moving_average <- function(focal_date, dates, concentrations, win_size_weeks) {
+  
+  # Which dates are in the window
+  is_in_window <- (dates > focal_date - (win_size_weeks / 2) * 7) * (dates < focal_date + (win_size_weeks / 2) * 7)
+  # Find the associated concentrations
+  window_conc <- conc[is_in_window]
+  # Calculate the mean
+  result <- mean(window_conc)
+  
+  return(result)
+  
 }
 
+ca_ma_1990_04_05 <- moving_average(focal_date = as.Date("1990-04-05"),
+                                   dates = tiny_problem$sample_date,
+                                   conc = tiny_problem$ca,
+                                   win_size_weeks = 9)
 
 # 5. Verify Your Solution -------------------------------------------------
 
 sheet_url <- "https://docs.google.com/spreadsheets/d/1X-SltNKUaZH0zVWSUkC8sUrrlhPEPAd-6tBmbcihCSw"
-tiny_problem <- googlesheets4::read_sheet(sheet_url, sheet = "Example")
+tiny_problem <- googlesheets4::read_sheet(sheet_url, sheet = "Group 8")
+tiny_problem$sample_date <- as.Date(tiny_problem$sample_date)
 
-one_case_solution <- your_function(...)
+ca_ma_once <- moving_average(focal_date = as.Date("1990-04-05"),
+                             dates = tiny_problem$sample_date,
+                             conc = tiny_problem$ca,
+                             win_size_weeks = 9)
 
 # Did it work? If not, what went wrong? Don't debug yet!
 
@@ -44,6 +63,16 @@ one_case_solution <- your_function(...)
 
 # _How_ will you apply your function?
 
+# Max wants to use sapply()
+
 # Write your full solution
+
+tiny_problem$calc_rolling <- sapply(
+  tiny_problem$sample_date,
+  moving_average,
+  dates = tiny_problem$sample_date,
+  conc = tiny_problem$ca,
+  win_size_weeks = 9
+)
 
 # Did it work? If not, what went wrong? Don't debug yet! 
